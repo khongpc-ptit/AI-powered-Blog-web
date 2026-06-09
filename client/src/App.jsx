7f750baaafde1c2727d2cfd8f372737b928e58f0
mhunglee20405 <mhunguit2005@gmail.com>
2026-06-10 00:05:43 +0700

    feat(fe):home,blog,admin pages

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import ListBlog from "./pages/admin/ListBlog";
+import AddBlog from "./pages/admin/AddBlog";
+import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
+import "quill/dist/quill.snow.css";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs/:id" element={<Blog />} />
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+export default App;
